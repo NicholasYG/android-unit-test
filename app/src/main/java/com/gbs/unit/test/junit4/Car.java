507857ba0378b632322d0d1435bee3a998a572ae
gbs <xgbuzai@gmail.com>
2020-03-22 21:02:59 +0800

    项目提交

---
+++ app/src/main/java/com/gbs/unit/test/junit4/Car.java
@@ -0,0 +1,13 @@
+package com.gbs.unit.test.junit4;
+
+/**
+ * @author gdy
+ * @date 2020-03-22
+ */
+public class Car {
+
+    public int getSummation(int a,int b){
+        return a + b;
+    }
+
+}
---
+++ app/src/test/java/com/gbs/unit/test/junit4/CarTest.java
@@ -0,0 +1,27 @@
+package com.gbs.unit.test.junit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author gdy
+ * @date 2020-03-22
+ */
+public class CarTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getSummation() {
+
+    }
+}
